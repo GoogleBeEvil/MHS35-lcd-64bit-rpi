91287f0fe1c7e16f9a551d1712d2ef8dbd636dff
GoogleBeEvil <39509734+GoogleBeEvil@users.noreply.github.com>
2021-10-29 11:31:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,11 +13,10 @@ echo "hdmi_force_hotplug=1" >> /boot/config.txt
 echo "dtparam=i2c_arm=on" >> /boot/config.txt
 echo "dtparam=spi=on" >> /boot/config.txt
 echo "enable_uart=1" >> /boot/config.txt
-echo "dtoverlay=mhs35:rotate=90" >> /boot/config.txt
+echo "dtoverlay=waveshare35c:rotate=270" >> /boot/config.txt
 echo "max_framebuffers=2" >> /boot/config.txt
 echo "arm_64bit=1" >> /boot/config.txt
-echo "hdmi_force_hotplug=1" >> /boot/config.txt
-echo "kernel=kernel8l-alt.img" >> /boot/config.txt
+echo "kernel=kernel8.img" >> /boot/config.txt
 echo "hdmi_group=1" >> /boot/config.txt
 echo "hdmi_mode=4" >> /boot/config.txt
 echo "hdmi_cvt 480 320 60 6 0 0 0" >> /boot/config.txt
